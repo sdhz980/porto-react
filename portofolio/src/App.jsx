4349237c6e840c2bbcd12780cb88ba7835a9026b
sdhz980 <sadamhack@gmail.com>
2024-03-05 15:18:55 +0700

    porto

---
+++ portofolio/src/App.jsx
@@ -0,0 +1,19 @@
+import Banner from './components/Banner'
+import Content from './components/Content'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <div>
+
+        <Navbar/>
+        <Banner/>
+        <Content/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
